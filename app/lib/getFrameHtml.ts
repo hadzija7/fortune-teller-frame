c709433918858fab0862695e1435b4668d99e391
hadzija7 <hadzijasale@gmail.com>
2024-08-03 17:42:40 +0200

    added first_question frame

--- app/lib/getFrameHtml.ts
+++ app/lib/getFrameHtml.ts
@@ -15,4 +15,23 @@ export function getInstructionsHtml(frameMetadata: FrameMetadataType){
 
     return `${html.slice(0, html.length - 14)}${extraTags.join('')}</head></html>`;
 }
+
+export function getFirstQuestionHtml(frameMetadata: FrameMetadataType){
+    const html = getFrameHtmlResponse(frameMetadata);
   
+    const extraTags = [
+      '<meta property="fc:frame" content="vNext" />',
+      `<meta name="fc:frame:image" content="https://${NEXT_PUBLIC_URL}/api/images/start" />`,
+      '<meta property="fc:frame:image:aspect_ratio" content="1:1" />',
+      '<meta property="fc:frame:button:1" content="Ethereum" />',
+      `<meta property="fc:frame:button:1:target" content="${NEXT_PUBLIC_URL}/api/first_question" />`,
+      '<meta property="fc:frame:button:2" content="Base" />',
+      `<meta property="fc:frame:button:2:target" content="${NEXT_PUBLIC_URL}/api/first_question" />`,
+      '<meta property="fc:frame:button:3" content="Optimism" />',
+      `<meta property="fc:frame:button:3:target" content="${NEXT_PUBLIC_URL}/api/first_question" />`,
+      '<meta property="fc:frame:button:4" content="Mantle" />',
+      `<meta property="fc:frame:button:4:target" content="${NEXT_PUBLIC_URL}/api/first_question" />`,
+    ];
+  
+    return `${html.slice(0, html.length - 14)}${extraTags.join('')}</head></html>`;
+  }
