13387381d46336918a8c46ae9bddfe2aa0723e5b
hadzija7 <hadzijasale@gmail.com>
2024-10-07 16:03:48 +0200

    contract update

--- contracts/AIGCNFT.sol
+++ contracts/AIGCNFT.sol
@@ -16,7 +16,7 @@ contract AIGCNFT is IERC7007, ERC721 { //IERC165, ERC721 {
 
     struct AIGCData {
         bytes fortune;
-        bytes imageCID; 
+        bytes imageCID;
         address author;
     }
 
@@ -50,15 +50,15 @@ contract AIGCNFT is IERC7007, ERC721 { //IERC165, ERC721 {
         totalSupply++;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        _requireOwned(tokenId);
-
+    function tokenURI(uint256 tokenId) override public view returns (string memory) {
         bytes memory prompt = tokenIdToPrompt[tokenId];
+        string memory json = Base64.encode(bytes(string(abi.encodePacked(
+            '{"image": "ipfs://', string(aigcData[prompt].imageCID),'"}'))));
+        // string memory json = Base64.encode(bytes(string(abi.encodePacked(
+        //     '{"name":"token#', toString(tokenId), '", "attributes": [{"trait_type": "fortune", "value": "', string(aigcData[prompt].fortune), '"}, {"trait_type": "author", "value": "0x', toAsciiString(aigcData[prompt].author), '"}, {"trait_type": "model", "value": "Stable Diffusion"}], "description": "FortuneTeller utilises ORAs AI Oracle to mint AIGC NFTs about each players fortune.", "image": "ipfs://', string(aigcData[prompt].imageCID), '"}'))));
+        string memory output = string(abi.encodePacked('data:application/json;base64,', json));
 
-        bytes memory cid = aigcData[prompt].imageCID;
-
-        string memory baseURI = "https://ipfs.io/ipfs/";
-        return bytes(baseURI).length > 0 ? string.concat(baseURI, string(cid)) : "";
+        return output;
     }
 
     function supportsInterface(
@@ -93,4 +93,104 @@ contract AIGCNFT is IERC7007, ERC721 { //IERC165, ERC721 {
         bytes calldata aigcData,
         bytes calldata proof
     ) external view returns (bool success) {}
+
+    function toString(uint256 value) internal pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT license
+        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
+    }
+}
+
+function toAsciiString(address x) pure returns (string memory) {
+    bytes memory s = new bytes(40);
+    for (uint i = 0; i < 20; i++) {
+        bytes1 b = bytes1(uint8(uint(uint160(x)) / (2**(8*(19 - i)))));
+        bytes1 hi = bytes1(uint8(b) / 16);
+        bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+        s[2*i] = char(hi);
+        s[2*i+1] = char(lo);            
+    }
+    return string(s);
+}
+
+function char(bytes1 b) pure returns (bytes1 c) {
+    if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+    else return bytes1(uint8(b) + 0x57);
+}
+
+/// [MIT License]
+/// @title Base64
+/// @notice Provides a function for encoding some bytes in base64
+/// @author Brecht Devos <brecht@loopring.org>
+library Base64 {
+    bytes internal constant TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    /// @notice Encodes some bytes to the base64 representation
+    function encode(bytes memory data) internal pure returns (string memory) {
+        uint256 len = data.length;
+        if (len == 0) return "";
+
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((len + 2) / 3);
+
+        // Add some extra buffer at the end
+        bytes memory result = new bytes(encodedLen + 32);
+
+        bytes memory table = TABLE;
+
+        assembly {
+            let tablePtr := add(table, 1)
+            let resultPtr := add(result, 32)
+
+            for {
+                let i := 0
+            } lt(i, len) {
+
+            } {
+                i := add(i, 3)
+                let input := and(mload(add(data, i)), 0xffffff)
+
+                let out := mload(add(tablePtr, and(shr(18, input), 0x3F)))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(shr(12, input), 0x3F))), 0xFF))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(shr(6, input), 0x3F))), 0xFF))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(input, 0x3F))), 0xFF))
+                out := shl(224, out)
+
+                mstore(resultPtr, out)
+
+                resultPtr := add(resultPtr, 4)
+            }
+
+            switch mod(len, 3)
+            case 1 {
+                mstore(sub(resultPtr, 2), shl(240, 0x3d3d))
+            }
+            case 2 {
+                mstore(sub(resultPtr, 1), shl(248, 0x3d))
+            }
+
+            mstore(result, encodedLen)
+        }
+
+        return string(result);
+    }
 }
